b320db240d18de070014c0a1d5182f0b59e2c511
alfredskeden <alfred.skedeback91@gmail.com>
2024-01-05 13:57:07 +0100

    feat(uuid): resturants filter uuid

--- src/data/filter.ts
+++ src/data/filter.ts
@@ -1,11 +1,11 @@
-import { Filter, createFilter } from '../models/filter';
+import { Filter, createFilter } from "../models/filter";
 
 export const mockFilterData: Filter[] = [
-  createFilter('Hamburger', 'https://i.imgur.com/m3vGiUZ.png'),
-  createFilter('Pizza', 'https://i.imgur.com/bobBnmk.png'),
-  createFilter('Taco´s', 'https://i.imgur.com/tH0EvOh.png'),
-  createFilter('Coffee', 'https://i.imgur.com/W4d883v.png'),
-  createFilter('Buritto', 'https://i.imgur.com/B83zjpk.png'),
-  createFilter('Fries', 'https://i.imgur.com/uqgCi8Y.png'),
-  createFilter('Breakfast', 'https://i.imgur.com/AevtNco.png'),
+  createFilter("Hamburger", "https://i.imgur.com/m3vGiUZ.png"),
+  createFilter("Pizza", "https://i.imgur.com/bobBnmk.png"),
+  createFilter("Taco´s", "https://i.imgur.com/tH0EvOh.png"),
+  createFilter("Coffee", "https://i.imgur.com/W4d883v.png"),
+  createFilter("Burrito", "https://i.imgur.com/B83zjpk.png"),
+  createFilter("Fries", "https://i.imgur.com/uqgCi8Y.png"),
+  createFilter("Breakfast", "https://i.imgur.com/AevtNco.png"),
 ];
--- src/data/restaurant.ts
+++ src/data/restaurant.ts
@@ -1,11 +1,12 @@
 import { Restaurant, createRestaurant } from '../models/restaurant';
+import { getFilterIdByName } from '../services/FilterService';
 
 export const mockRestaurantData: Restaurant[] = [
-  createRestaurant('Waynes Coffee', 4.5, ['Coffee'], 'https://i.imgur.com/W4d883v.png', 30),
-  createRestaurant('Oskars Tacos', 3.8, ['Taco´s'], 'https://i.imgur.com/tH0EvOh.png', 45),
-  createRestaurant('Dawids Deli', 4.9, ['Fries', 'Burrito'], 'https://i.imgur.com/uqgCi8Y.png', 60),
-  createRestaurant('Viktors Valmofrön & Potatis', 4.2, ['Pizza', 'Fries'], 'https://i.imgur.com/uqgCi8Y.png', 30),
-  createRestaurant('Sebbes Slizes', 4.3, ['Pizza',], 'https://i.imgur.com/bobBnmk.png', 45),
-  createRestaurant('Karls Korv (vegan)', 4.4, ['Breakfast'], 'https://i.imgur.com/AevtNco.png', 20),
-  createRestaurant('Emils Elit-biffar', 4.5, ['Hamburger'], 'https://i.imgur.com/m3vGiUZ.png', 60),
+  createRestaurant('Waynes Coffee', 4.5, [getFilterIdByName('Coffee')], 'https://i.imgur.com/W4d883v.png', 30),
+  createRestaurant('Oskars Tacos', 3.8, [getFilterIdByName('Taco´s')], 'https://i.imgur.com/tH0EvOh.png', 45),
+  createRestaurant('Dawids Deli', 4.9, [getFilterIdByName('Fries'), getFilterIdByName('Burrito')], 'https://i.imgur.com/uqgCi8Y.png', 60),
+  createRestaurant('Viktors Valmofrön & Potatis', 4.2, [getFilterIdByName('Pizza'), getFilterIdByName('Fries')], 'https://i.imgur.com/uqgCi8Y.png', 30),
+  createRestaurant('Sebbes Slizes', 4.3, [getFilterIdByName('Pizza')], 'https://i.imgur.com/bobBnmk.png', 45),
+  createRestaurant('Karls Korv (vegan)', 4.4, [getFilterIdByName('Breakfast')], 'https://i.imgur.com/AevtNco.png', 20),
+  createRestaurant('Emils Elit-biffar', 4.5, [getFilterIdByName('Hamburger')], 'https://i.imgur.com/m3vGiUZ.png', 60),
 ];
