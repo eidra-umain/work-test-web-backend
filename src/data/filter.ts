5a3ec6bff4b8a2f136a02a9c4f4b3b49f96c2373
alfredskeden <alfred.skedeback91@gmail.com>
2024-01-05 16:12:29 +0100

    update(fixes): update name of images and filter images urls

--- src/data/filter.ts
+++ src/data/filter.ts
@@ -3,30 +3,30 @@ import { Filter, createFilter } from '../models/filter';
 export const mockFilterData: Filter[] = [
   createFilter({
     name: 'Hamburger',
-    image_url: '',
+    image_url: '/images/hamburger.png',
   }),
   createFilter({
     name: 'Pizza',
-    image_url: 'https://i.imgur.com/bobBnmk.png',
+    image_url: '/images/pizza.png',
   }),
   createFilter({
     name: 'Taco´s',
-    image_url: 'https://i.imgur.com/tH0EvOh.png',
+    image_url: '/images/taco.png',
   }),
   createFilter({
     name: 'Coffee',
-    image_url: 'https://i.imgur.com/W4d883v.png',
+    image_url: '/images/coffee.png',
   }),
   createFilter({
     name: 'Burrito',
-    image_url: 'https://i.imgur.com/B83zjpk.png',
+    image_url: '/images/burrito.png',
   }),
   createFilter({
     name: 'Fries',
-    image_url: 'https://i.imgur.com/uqgCi8Y.png',
+    image_url: '/images/fries.png',
   }),
   createFilter({
     name: 'Breakfast',
-    image_url: 'https://i.imgur.com/AevtNco.png',
+    image_url: '/images/breakfast.png',
   }),
 ];
