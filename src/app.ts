c1f7e8036d78d2cc2da1c2672343b5e1eded8ddd
alfredskeden <alfred.skedeback91@gmail.com>
2024-01-05 15:00:29 +0100

    fix(images): update on ticket #2 fix static serving of public and links

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import routes from "./routes/routes";
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(express.static("public"));
+
 app.use("/api", routes);
 
 const specs = swaggerJsdoc(swaggerOptions);
