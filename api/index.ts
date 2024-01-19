63865636f7bf8f08ee501408fcd0ba4de60c453b
Viktor Åsbrink <viktor.asbrink@umain.com>
2024-01-19 16:34:17 +0100

    Test update file structure for vercel hosting

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import swaggerOptions from '../src/swagger/swaggerOptions';
+import routes from '../src/routes/routes';
+
+const app = express();
+const PORT = process.env.PORT ?? 80;
+
+if (process.env.NODE_ENV == 'development') {
+  app.use(express.static('public'));
+}
+
+app.use('/api', routes);
+
+const specs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+export default app;
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,9 @@
-import express from 'express';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import swaggerOptions from './swagger/swaggerOptions';
-import routes from './routes/routes';
 import dotenv from 'dotenv';
+import app from '../api/';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT ?? 80;
-
-if (process.env.NODE_ENV == 'development') {
-  app.use(express.static('public'));
-}
-
-app.use('/api', routes);
-
-const specs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
