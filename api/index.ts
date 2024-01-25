f1fbfa54727fe4dd36d8c777eb7a98e18bf5e57f
Viktor Åsbrink <viktor.asbrink@umain.com>
2024-01-25 13:10:04 +0100

    More testing

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import swaggerOptions from '../swagger/swaggerOptions';
-import routes from '../routes/routes';
+import swaggerOptions from '../src/swagger/swaggerOptions';
+import routes from '../src/routes/routes';
 
 const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.use('/api', routes);
 
 const specs = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-app.use('/', (req, res) => res.send('hello'));
+app.get('/', (req, res) => res.send('hello'));
 
 export default app;
